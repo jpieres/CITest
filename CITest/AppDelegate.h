27ef67eff2585876e67dc97585b4354c614dec43
jpieres <juan.pieres@sage.com>
2015-06-01 17:30:03 -0400

    Initial commit

---
+++ CITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CITest
+//
+//  Created by Juan Pieres on 01/06/15.
+//  Copyright (c) 2015 Juan Pieres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CITest
+//
+//  Created by Juan Pieres on 01/06/15.
+//  Copyright (c) 2015 Juan Pieres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
